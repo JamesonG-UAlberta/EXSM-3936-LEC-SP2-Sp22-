87a55cfbf4829a0931d2af2e8cc9857be44fd4e6
James Grieve <jgrieve@ualberta.ca>
2022-05-11 20:23:53 -0600

    Whitespace validation; Clear text field on add;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,16 @@
 const theInput = document.querySelector("input");
 const theButton = document.querySelector("button");
 const theList = document.querySelector("ul");
+theInput.focus();
 
 function runThisWhenWeClickTheButton() {
-    const newLI = document.createElement("li");
-    newLI.innerText = theInput.value;
-    theList.appendChild(newLI);
+    if (theInput.value.trim() !== "") {
+        const newLI = document.createElement("li");
+        newLI.innerText = theInput.value.trim();
+        theInput.value = "";
+        theList.appendChild(newLI);
+        theInput.focus();
+    }
 }
 
 theButton.addEventListener("click", runThisWhenWeClickTheButton);
